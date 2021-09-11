ff28bc0e684f4de4c8e4d98c12f4bf86674a0bd3
raviansyahyp <raviansyahy@gmail.com>
2021-09-11 18:11:46 +0700

    praktikum3

--- app/Http/Controllers/CommentController.php
+++ app/Http/Controllers/CommentController.php
@@ -6,5 +6,8 @@ use Illuminate\Http\Request;
 
 class CommentController extends Controller
 {
-    //
+    public function comment($nama, $pesan) {
+        return "Nama: '$nama' <br>
+            Pesan: $pesan";
+    }
 }
